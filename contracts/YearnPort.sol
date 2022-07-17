f38161c5faefbd53f4c5aab15a06d9e5a0090233
Chuk <65512186+Malinariy@users.noreply.github.com>
2022-07-17 22:34:30 +0200

    Update YearnPort.sol
    Add deposit and withdraw functions

--- contracts/YearnPort.sol
+++ contracts/YearnPort.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
-import {SafeTransferLib} from "../lib/solmate/src/utils/SafeTransferLib.sol";
+import {ERC20} from "./../lib/solmate/src/tokens/ERC20.sol";
+import {SafeTransferLib} from "./../lib/solmate/src/utils/SafeTransferLib.sol";
 
 import {Factory} from "./Factory.sol";
-import {IYearn} from "../interfaces/Yearn/IYearn.sol";
+import {IYearn} from "./../interfaces/IYearn.sol";
 
     /*
     * @title YearnPort
@@ -43,25 +44,37 @@ contract YearnPort {
      * Getters
      */
 
+    // @return The underlying token in the YearnVault
     function getYearnUnderlyingToken(address vault) public view returns(ERC20){
-        // @return The underlying token in the YearnVault
         return ERC20(IYearn(vault).token());
     }
 
+    // @notice Computes the latest yield value for a yearn vault.
+    // @return The latest yield value
     function getYearnPricePerShare(address vault) public view returns(uint256){
-        // @notice Computes the latest yield value for a yearn vault.
-        // @return The latest yield value
         IYearn yearnVault = IYearn(vault);
         return (yearnVault.pricePerShare());
     }
+
 
 
     function setSomething(uint256 _something) external onlyFactory {
       something = _something;
     }
 
-    function deposit() external {
+    // @notice Deposit underlying into vault
+    function deposit(uint256 underlyingAmount, address vault) external returns (uint256){
+        return IYearn(vault).deposit(underlyingAmount);
     }
+
+    // @notice Converts vault shares to underlying tokens.
+    function withdraw(uint256 vaultSharesAmount, address recipient, address vault) external returns(uint256){
+        if (vaultSharesAmount == 0) {
+            return 0;
+        }
+         return IYearn(vault).withdraw(vaultSharesAmount, recipient);
+    }
+
 
     function _invest() internal {
       // берет текущие балансы accepted_tokens и депозитит фармиться
